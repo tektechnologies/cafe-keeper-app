3633e909cfd4100738ad4787081283a7fa42a489
tektechnologies <craigwbarkley@icloud.com>
2026-03-09 19:04:25 -0500

    move page components to index.tsx to render app in the root route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import Header from '../components/Navs/Header'
+import Banner from '@/components/Banner'
+import Footer from '@/components/Footer'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  
-  
   return (
-   
-      
-    
-       <>hello</>
+    <>
+      <Header />
+      <Banner />
+      <Footer />
+    </>
   )
 }
