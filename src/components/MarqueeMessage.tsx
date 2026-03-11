2f2e5952da576f653912b287182c7dadb155b2d7
tektechnologies <craigwbarkley@icloud.com>
2026-03-11 18:44:18 -0500

    add marquee and library

---
+++ src/components/MarqueeMessage.tsx
@@ -0,0 +1,14 @@
+import Marquee from "react-fast-marquee"
+ 
+ 
+ export default function MarqueeMessage() {
+  return (
+    <>
+       <Marquee>
+      <span className="mx-8">🔥 Fresh Coffee</span>
+      <span className="mx-8">☕ Locally Roasted Beans</span>
+      <span className="mx-8">🥐 Fresh Pastries Daily</span>
+    </Marquee>
+    </>
+  )
+}
