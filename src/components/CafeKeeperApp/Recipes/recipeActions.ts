7d105d9110c98f1bcb6261386e9a4955fe64001a
tektechnologies <craigwbarkley@icloud.com>
2026-03-26 17:04:13 -0500

    add recipeActions POST call

---
+++ src/components/CafeKeeperApp/Recipes/recipeActions.ts
@@ -0,0 +1,47 @@
+import { createServerFn } from '@tanstack/react-start'
+import { db } from '../../../db/client'
+import { ingredients, recipe_ingredients, recipe_tags, recipes, steps, tags } from '../../../db/schema'
+import type { NormalizedRecipePayload } from './AddRecipes'
+
+
+
+export const saveRecipe = createServerFn({ method: 'POST' })
+  .handler(async (ctx) => {
+    const data = ctx.data as unknown as NormalizedRecipePayload
+
+    const [recipe] = await db.insert(recipes).values(data.recipe).returning()
+
+    if (data.steps.length) {
+      await db.insert(steps).values(
+        data.steps.map((s) => ({ ...s, recipe_id: recipe.id }))
+      )
+    }
+
+    for (const ing of data.ingredients) {
+      const [ingredient] = await db
+        .insert(ingredients)
+        .values({ name: ing.name })
+        .onConflictDoUpdate({ target: ingredients.name, set: { name: ing.name } })
+        .returning()
+
+      await db.insert(recipe_ingredients).values({
+        recipe_id:     recipe.id,
+        ingredient_id: ingredient.id,
+        quantity:      ing.quantity?.toString(),
+        unit:          ing.unit,
+        note:          ing.note,
+      })
+    }
+
+    for (const tagName of data.tagNames) {
+      const [tag] = await db
+        .insert(tags)
+        .values({ name: tagName })
+        .onConflictDoUpdate({ target: tags.name, set: { name: tagName } })
+        .returning()
+
+      await db.insert(recipe_tags).values({ recipe_id: recipe.id, tag_id: tag.id })
+    }
+
+    return recipe
+  })
